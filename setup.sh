3ec98536bcec622df4fb4e0b072206410899bc5c
Federico Pardo <fpg23feb98@gmail.com>
2018-05-19 10:18:36 +0200

    Telegram repositories no longer necessary

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ sudo apt upgrade -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-#Get Telegram Repository
-sudo add-apt-repository ppa:atareao/telegram
-
 #Add the Spotify repository signing keys and Repository
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -18,11 +15,6 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 #Get Key for SublimeText 3 and add Repository
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-#Add TeXstudio repository and install latex
-sudo apt install texlive-full
-sudo apt install texstudio
-sudo apt-get install texlive-lang-spanish
 
 #Add Java 10 Repository
 sudo add-apt-repository ppa:linuxuprising/java
@@ -34,6 +26,9 @@ sudo apt-get update
 sudo apt-get install google-chrome-stable -y
 sudo apt install flex -y
 sudo apt install bison -y
+sudo apt install texlive-full
+sudo apt install texstudio
+sudo apt-get install texlive-lang-spanish
 sudo apt-get install telegram-desktop -y
 sudo apt-get install spotify-client -y
 sudo apt-get install sublime-text -y
