e90b667524ec35589b18c7f95df6afb473dc3ce4
Alejandro <alexiando29@gmail.com>
2018-07-23 13:23:42 +0200

    Solucionados todos los problemas y vuelto a añadir GitKraken

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 # Instalar curl para poder añadir todos los repositorios (no viene por defecto en la instalación mínima de Ubuntu)
 sudo apt -y install curl
 
-# Repositorio para Telegram
-sudo add-apt-repository ppa:atareao/telegram
 
 # Repositorio para Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
@@ -30,14 +28,22 @@ sudo apt -y upgrade
 
 # Instalar los programas
 sudo apt -y install google-chrome-stable
-sudo apt -y install texstudio texlive-lang-spanish texlive-latex-extra cm-super
-sudo apt -y install telegram
+sudo apt -y install texstudio texlive-base texlive-lang-spanish texlive-latex-extra cm-super
+sudo apt -y install telegram-desktop
 sudo apt -y install spotify-client
 sudo apt -y install openjdk-8-jre openjdk-8-jre-dcevm openjdk-8-dbg openjdk-8-jre-headless openjdk-8-demo openjdk-8-jre-zero openjdk-8-doc openjdk-8-source openjdk-8-jdk openjdk-8-jdk-headless
 sudo apt -y install git gcc g++ vim
 sudo apt -y install atom code
+#Paquetes para GitKraken
+sudo apt -y install libgnome-keyring-common libgnome-keyring0
+#Paquetes para Discord
 sudo apt -y install libappindicator1 libc++1 libc++abi1 libindicator7
 
+
+# Instalar GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+rm gitkraken-amd64.deb
 
 # Instalar Discord
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
