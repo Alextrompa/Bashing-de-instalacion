401beb4af4305656112fc371a8b689acb3710b7c
Alejandro <alejandro@archAlex.local>
2021-09-05 00:22:48 +0200

    Titulos scripts

---
+++ Fedora.sh
@@ -0,0 +1,49 @@
+#Actualizamos todos los paquetes
+sudo dnf -y update
+
+#Habilitamos el repositorio de paquetes RPM Fusion
+sudo dnf -y install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y groupupdate core
+
+#Descargamos plugins para música y vídeo
+sudo dnf -y install gstreamer1-libav gstreamer1-plugins-bad-free \
+       gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras \
+       gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-nonfree \
+       gstreamer1-plugins-good gstreamer1-plugins-ugly lame-libs lame-libs
+sudo dnf -y group upgrade --with-optional Multimedia
+
+#Añadimos el flathub a los repositorios de flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#Añadimos el repositorio para Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+
+#Descargamos el paquete de idiomas español
+sudo dnf -y install hunspell-es-ES
+
+#Instalamos las aplicaciones deseadas
+sudo dnf -y install python java-1.8.0-openjdk make meld vim qemu code gcc g++ R unrar
+sudo dnf -y install rstudio-desktop
+sudo dnf -y install texstudio texlive-base texlive-collection-latexextra texlive-glossaries-spanish texlive-collection-langspanish
+
+#Instalamos las aplicaciones deseadas
+sudo flatpak install flathub com.spotify.Client com.discordapp.Discord org.telegram.desktop
+
+#GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.rpm
+sudo rpm -i gitkraken-amd64.rpm
+rm gitkraken-amd64.rpm
+
+#Google Chrome
+wget -O googlechrome.rpm "https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
+sudo dnf install -y ./googlechrome.rpm
+rm googlechrome.rpm
+
+#Limpieza de ficheros
+sudo dnf -y autoremove
+sudo dnf clean all
---
+++ Ubuntu.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -u
+
+# Instalar curl para poder añadir todos los repositorios (no viene por defecto en la instalación mínima de Ubuntu)
+sudo apt -y install curl
+
+# Repositorio para Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# Repositorio para Visual Studio Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Actualizar paquetes
+sudo apt update
+sudo apt -y upgrade
+
+# Instalar los programas
+sudo apt -y install texstudio texlive-base texlive-lang-spanish texlive-latex-extra cm-super
+sudo apt -y install telegram-desktop
+sudo apt -y install spotify-client
+sudo apt -y install openjdk-8-jre openjdk-8-jre-dcevm openjdk-8-dbg openjdk-8-jre-headless openjdk-8-demo openjdk-8-jre-zero openjdk-8-doc openjdk-8-source openjdk-8-jdk openjdk-8-jdk-headless
+sudo apt -y install git gcc g++ vim make meld r-base
+sudo apt -y install code
+sudo apt -y install net-tools
+sudo apt -y install rar unrar
+#Paquetes para GitKraken
+sudo apt -y install libgnome-keyring-common libgnome-keyring0
+#Paquetes para Discord
+sudo apt -y install libappindicator1 libc++1 libc++abi1 libindicator7
+
+# Instalar Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Instalar GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+rm gitkraken-amd64.deb
+
+# Instalar Discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+rm discord.deb
+
+# Instalar RStudio
+wget -O rstudio.deb "https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb"
+sudo apt -y install ./rstudio.deb
+rm rstudio.deb
+
+# Limpieza de ficheros
+sudo apt autoclean
+sudo apt clean all
+sudo apt autoremove
